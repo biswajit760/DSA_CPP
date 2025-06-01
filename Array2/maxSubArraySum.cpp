efe43025c7e646e75ad57e6c41e9b7aacd1b3dc8
Biswajit Mahanty <155170520+biswajit760@users.noreply.github.com>
2025-06-01 12:23:29 +0530

    1/06/25 | | Topic : Array

--- Array2/maxSubArraySum.cpp
+++ Array2/maxSubArraySum.cpp
@@ -18,12 +18,42 @@ int maxSubarraySum(int *arr, int n){
     cout << "Max Sub Array Sum = " << maxSum << endl;
 }
 
+// Optimal Solution
+
+int maxSubarraySum2(int *arr, int n){
+    int maxSum  = INT8_MIN;
+    for(int st=0; st<n; st++){
+        int currSum =0;
+        for(int end = st; end<n; end++){
+            
+            currSum += arr[end];
+            maxSum = max(maxSum, currSum);
+        }
+
+        cout << endl;
+    }
+    cout << "Max Sub Array Sum = " << maxSum << endl;
+}
+
+// Kadanes Algorithm
+void maxSubarraySum3(int *arr, int n){
+    int maxSum  = INT8_MIN;
+    int currSum = 0;
+    for(int i =0; i< n; i++){
+        currSum += arr[i];
+        maxSum = max(currSum, maxSum);
+        if(currSum < 0){ // -ve value
+            currSum = 0;
+        }
+    }
+     cout << "Max Sub Array Sum by Kadanes Algorithm = " << maxSum << endl;
+}
 int main(){
     int arr[6] = {2, -3, 6, -5, 4, 2};
 
     int n = sizeof(arr)/sizeof(int);
 
-    maxSubarraySum(arr, n);
+    maxSubarraySum3(arr, n);
 
     return 0; 
 }
